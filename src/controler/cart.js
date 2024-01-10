c81fb8a3847de9c2033f9dced4b11a60f2b25c77
Urvish patel <urvishvpatel199@gmail.com>
2024-01-10 23:52:08 +0530

    add data

--- src/controler/cart.js
+++ src/controler/cart.js
@@ -1,3 +1,4 @@
+import { VirtualAgent } from "twilio/lib/twiml/VoiceResponse";
 import { model } from "../models";
 
 export const getall = (req, res) => {
@@ -12,18 +13,48 @@ export const getall = (req, res) => {
 };
 
 export const create = async (req, res) => {
-  console.log("req?.loginUser", req?.loginUser);
-  model.Cart.update(
+  const productId = req?.params?.id
+  console.log("req?.loginUser?._id", req?.loginUser?._id)
+  const match = await model.Cart.findOne(
     { userId: req?.loginUser?._id },
-    { ...req?.body, userId: req?.loginUser?.id },
-    { upsert: true }
+    // { "products": { $exists: true, $not: { $size: 0 } } }
   )
-    .then((resData) => {
-      res.send({ status: 200, data: resData });
+  console.log("match", match)
+  if (match) {
+    let index = match?.products?.findIndex((e) => {
+      console.log(e?.productId.toString(), productId)
+      return e?.productId.toString() === productId
     })
-    .catch((err) => {
-      res.send({ status: 400, message: err.message });
-    });
+    if (index === -1) {
+      console.log("----")
+      match.products.push({ productId: productId, count: 1 })
+    } else {
+      console.log("=====")
+      match.products[index].count += 1
+    }
+    console.log("---index", index)
+    // console.log()
+    model?.Cart?.findByIdAndUpdate(match?._id, match, { new: true }).
+      then((resData) => {
+        res.send({ status: 200, data: resData });
+      })
+      .catch((err) => {
+        res.send({ status: 400, message: err.message });
+      });
+
+  } else {
+    model.Cart.create(
+      { userId: req?.loginUser?._id, products: [{ productId, count: 1 }] },
+    ).
+      then((resData) => {
+        res.send({ status: 200, data: resData });
+      })
+      .catch((err) => {
+        res.send({ status: 400, message: err.message });
+      });
+
+  }
+
 };
 
 export const update = (req, res) => {
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import GoogkleAuth from "./GoogleAuth/googleAuth";
 import http from "http";
 const app = express();
 const server = http.createServer(app);
+import { json } from "body-parser";
 
 import { Server } from "socket.io";
 import { cronJob } from "./functions/cronJob";
@@ -35,8 +36,10 @@ app.get("/login", (req, res) => {
   res.send("login faild....!");
 });
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
+app.use("/", json());
 app.use("/user", Routes.userRoute);
 app.use("/product", Routes.productRoute);
 app.use("/cart", Routes.cartRouter);
